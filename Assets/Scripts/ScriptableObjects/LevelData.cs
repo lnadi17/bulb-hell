59436c8d1469105b99c5857d8025b6f0200cfdd7
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2021-04-12 22:51:40 +0400

    Create custom drawer for level data

--- Assets/Scripts/ScriptableObjects/LevelData.cs
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Sirenix.Utilities;
+using UnityEditor.Experimental.Networking.PlayerConnection;
 using UnityEngine;
 
 namespace ScriptableObjects {
@@ -11,35 +14,55 @@ namespace ScriptableObjects {
         public Level[] levels;
 
         private Level AddLevelWithNextIndex() {
-            return new Level {index = levels.Length};
+            var createdLevel = new Level {index = levels.Length, size = 5};
+            createdLevel.Initialize();
+            return createdLevel;
         }
-
-        [Serializable]
-        public class Level {
-            [FoldoutGroup("$Name", false)] public int index = 0;
+        
+        public struct Level {
+            [FoldoutGroup("$Name", false)] public int index;
 
             [FoldoutGroup("$Name", false)] [Range(2, 10), OnValueChanged("Initialize")]
-            public int size = 5;
+            public int size;
 
-            [FoldoutGroup("$Name", false)] [ShowInInspector] [TableList(AlwaysExpanded = true, ShowIndexLabels = true)]
-            public List<List<bool>> matrix;
+            [FoldoutGroup("$Name", false)]
+            [TableMatrix(HorizontalTitle = "Board", DrawElementMethod = "DrawColoredElement", SquareCells = true)]
+            public bool[,] matrix;
 
             public string Name => $"Level {index + 1}";
 
-            public Level() {
-                Initialize();
+            private List<List<bool>> _matrixList;
+
+            public List<List<bool>> Matrix {
+                get => _matrixList;
+                set => _matrixList = value;
             }
 
             public void Initialize() {
-                matrix = new List<List<bool>>(size);
+                _matrixList = new List<List<bool>>(size);
+                matrix = new bool[size, size];
                 for (var i = 0; i < size; i++) {
                     var row = new List<bool>(size);
                     for (var j = 0; j < size; j++) {
                         row.Add(false);
+                        matrix[i, j] = false;
                     }
 
-                    matrix.Add(row);
+                    _matrixList.Add(row);
                 }
+            }
+            
+            private static bool DrawColoredElement(Rect rect, bool value) {
+                if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition)) {
+                    value = !value;
+                    GUI.changed = true;
+                    Event.current.Use();
+                }
+
+                UnityEditor.EditorGUI.DrawRect(rect.Padding(1),
+                    value ? new Color(1f, 1f, 0.28f) : new Color(0.02f, 0.01f, 0.01f, 0.82f));
+
+                return value;
             }
         }
     }
